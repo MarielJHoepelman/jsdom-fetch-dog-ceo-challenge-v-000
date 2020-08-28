c70f5fa950754824c0009c893b6988de4c35bc8c
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-28 03:03:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,37 +11,42 @@ document.addEventListener('DOMContentLoaded', function() {
       })
     }
 
-    fetchUrl("https://dog.ceo/api/breeds/image/random/4").then(function(json){
-      for (const element of json.message) {
-        let dogContainer = document.getElementById("dog-image-container")
-        let image = document.createElement("img");
-        image.setAttribute("width", "304");
-        image.setAttribute("src", element);
-        dogContainer.appendChild(image);
+  fetchUrl("https://dog.ceo/api/breeds/image/random/4").then(function(json){
+    for (const element of json.message) {
+      let dogContainer = document.getElementById("dog-image-container")
+      let image = document.createElement("img");
+      image.setAttribute("width", "304");
+      image.setAttribute("src", element);
+      dogContainer.appendChild(image);
+    }
+  })
+
+  let list = document.getElementById("dog-breeds")
+
+  fetchUrl("https://dog.ceo/api/breeds/list/all").then(function(json){
+    let breeds = Object.keys(json.message)
+
+    for (const element of breeds) {
+      let li = document.createElement("li");
+      li.innerHTML = element
+      list.appendChild(li)
+    }
+
+    list.addEventListener('click', event => {
+      if (event.target.nodeName === "LI"){
+        event.target.style.color = "pink";
       }
     })
 
-      fetchUrl("https://dog.ceo/api/breeds/list/all").then(function(json){
-        let breeds = Object.keys(json.message)
-        let list = document.getElementById("dog-breeds")
+  })
 
-        for (const element of breeds) {
-          let li = document.createElement("li");
-          li.innerHTML = element
-          list.appendChild(li)
-        }
-
-        list.addEventListener('click', event => {
-          if (event.target.nodeName === "LI"){
-            event.target.style.color = "pink";
-          }
-        })
-
-
-
-
-
-      })
+  function renderBreeds(breeds){
+    for (const element of breeds) {
+      let li = document.createElement("li");
+      li.innerHTML = element
+      list.appendChild(li)
+    }
+  }
 
 
 })
