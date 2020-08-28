64b70a9de0f5a0dc9d6ba302b9e35c413bdbe749
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-28 02:50:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('%c HI', 'color: firebrick')
 
+  function fetchUrl(url) {
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json){
+        return json
+      })
+    }
+
   function fetchImages() {
     return fetch("https://dog.ceo/api/breeds/image/random/4")
       .then(function(response) {
