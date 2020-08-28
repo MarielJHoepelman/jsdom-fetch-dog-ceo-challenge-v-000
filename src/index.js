22e98d6b2b94ec61b356d0a02066132233c6d970
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-28 02:47:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         list.addEventListener('click', event => {
           debugger;
-          if (event.target.nodeName === "li"){
+          if (event.target.nodeName === "LI"){
             event.target.style.color = "red";
           }
         })
